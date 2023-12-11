36d8f884016df2dc12c65f4dbfc72ea1075b5f4e
Robert Sturla <robertsturla@outlook.com>
2023-12-11 18:18:26 +0000

    Resolve prettier errors

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,19 +1,19 @@
 // @ts-check
 
-const isDevelopment = process.env.NODE_ENV === 'development';
+const isDevelopment = process.env.NODE_ENV === "development";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: isDevelopment ? 'standalone' : 'export',
-  distDir: 'dist',
+  output: isDevelopment ? "standalone" : "export",
+  distDir: "dist",
   // Use a function to conditionally set rewrites only in development
   ...(isDevelopment && {
     async rewrites() {
       return [
         {
-          source: '/api/:path*',
-          destination: 'http://localhost:8080/api/:path*',
+          source: "/api/:path*",
+          destination: "http://localhost:8080/api/:path*",
         },
       ];
     },

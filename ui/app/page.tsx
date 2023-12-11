4afde851e0a7563a8dee1d5a9b1b08821ec85cf8
Robert Sturla <robertsturla@outlook.com>
2023-12-11 15:50:48 +0000

    feature: create sample NextJS application

---
+++ ui/app/page.tsx
@@ -0,0 +1,23 @@
+'use client'
+import useSWR from 'swr'
+
+async function fetcher(url: string) {
+  const resp = await fetch(url);
+  return resp.text();
+}
+
+export default function Home() {
+  const { data, error } = useSWR('/api', fetcher, { refreshInterval: 1000 });
+
+  return (
+    <main>
+      <div>
+        <h1>Hello World!</h1>
+        <h2>Memory allocation stats from GoLang API</h2>
+        {error && <div>Failed to load: <strong>{error}</strong></div>}
+        {!error && !data && <p>Loading...</p>}
+        {!error && data && <pre>{data}</pre>}
+      </div>
+    </main>
+  )
+}

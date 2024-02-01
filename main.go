27c8955de97c57b4d6057ef956f7bdf81627db7a
Robert Sturla <robertsturla@outlook.com>
2024-02-01 00:52:54 +0000

    fix: add tiny main abstraction (#52)
    * Add main abstraction
    
    * Return s.ListenAndServe error in run func
    
    * Remove missing enum exit value
    
    * Remove unused fmt import

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"embed"
-	"fmt"
 	"github.com/rsturla/golang-aio/internal/http"
 	"github.com/rsturla/golang-aio/pkg/log"
 	"github.com/sirupsen/logrus"
@@ -13,6 +12,13 @@ import (
 var embedFS embed.FS
 
 func main() {
+	if err := run(os.Args); err != nil {
+		log.Errorf("Run failed with error: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(args []string) error {
 	// Set logging format based on environment.
 	if os.Getenv("ENVIRONMENT") == "development" {
 		log.SetLogLevel(logrus.DebugLevel)
@@ -27,7 +33,6 @@ func main() {
 
 	// Start the HTTP server.
 	addr := ":8080"
-	serverURL := fmt.Sprintf("http://localhost%s", addr)
-	log.Printf("Starting HTTP server at %s ...", serverURL)
-	log.Fatal(s.ListenAndServe(addr))
+	log.Printf("Starting HTTP server on port %s ...\n", addr)
+	return s.ListenAndServe(addr)
 }

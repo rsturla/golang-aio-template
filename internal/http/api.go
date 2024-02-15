d8f9f6385bdaf783a51536cf2e93b0bd232ea1d0
Robert Sturla <robertsturla@outlook.com>
2024-02-15 15:36:42 +0000

    Use correct log level for errors

--- internal/http/api.go
+++ internal/http/api.go
@@ -10,7 +10,7 @@ import (
 func (s *Server) handleAPI() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := writeAllocsProfile(w); err != nil {
-			log.Printf("Error: Failed to write allocs profile: %v", err)
+			log.Errorf("Failed to write allocs profile: %v", err)
 		}
 	}
 }

3dd30ffb34d4e973cd91b9e23de65f699aa372cb
Robert Sturla <robertsturla@outlook.com>
2024-02-19 11:57:39 +0000

    Fix mobile styling

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/knadh/koanf/providers/file v0.1.0
 	github.com/knadh/koanf/v2 v2.1.0
 	github.com/sirupsen/logrus v1.9.3
+	github.com/spf13/cobra v1.8.0
 	github.com/stretchr/testify v1.8.4
 )
 
@@ -20,7 +21,6 @@ require (
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/spf13/cobra v1.8.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- web/components/main-nav.tsx
+++ web/components/main-nav.tsx
@@ -1,55 +1,52 @@
-import { MainNavItem } from "@/lib/types"
-import { useSelectedLayoutSegment } from "next/navigation"
-import * as React from "react"
-import Link from "next/link"
-import { Icons } from "@/components/icons"
-import { siteConfig } from "@/config/site"
-import { cn } from "@/lib/utils"
-import { MobileNav } from "@/components/mobile-nav"
+import { MainNavItem } from "@/lib/types";
+import { useSelectedLayoutSegment } from "next/navigation";
+import * as React from "react";
+import Link from "next/link";
+import { Icons } from "@/components/icons";
+import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
+import { MobileNav } from "@/components/mobile-nav";
 
 interface MainNavProps {
-  items?: MainNavItem[]
-  children?: React.ReactNode
+  items?: MainNavItem[];
+  children?: React.ReactNode;
 }
 
 export function MainNav({ items, children }: MainNavProps) {
-  const segment = useSelectedLayoutSegment()
-  const [showMobileMenu, setShowMobileMenu] = React.useState<boolean>(false)
+  const segment = useSelectedLayoutSegment();
+  const [showMobileMenu, setShowMobileMenu] = React.useState<boolean>(false);
 
   return (
-    <div className="flex gap-6 md:gap-10">
-      <Link href="/" className="hidden items-center space-x-2 md:flex">
-        <Icons.logo />
-        <span className="hidden font-bold sm:inline-block">
-          {siteConfig.name}
-        </span>
-      </Link>
-      {items?.length ? (
-        <nav className="hidden gap-6 md:flex">
-          {items?.map((item, index) => (
-            <Link
-              key={index}
-              href={item.disabled ? "#" : item.href}
-              className={cn(
-                "flex items-center text-lg font-medium transition-colors hover:text-foreground/80 sm:text-sm",
-                item.href.startsWith(`/${segment}`)
-                  ? "text-foreground"
-                  : "text-foreground/60",
-                item.disabled && "cursor-not-allowed opacity-80"
-              )}
-            >
-              {item.title}
-            </Link>
-          ))}
-        </nav>
-      ) : null}
-      <div className="md:hidden flex">
+    <div className="flex h-16 items-center w-full">
+      {/* LEFT */}
+      <div className="flex w-6/12 space-x-8">
         <Link href="/" className="flex items-center space-x-2">
           <Icons.logo />
-          <span className="font-bold">
-            {siteConfig.name}
-          </span>
+          <span className="font-bold">{siteConfig.name}</span>
         </Link>
+        {items?.length ? (
+          <nav className="hidden gap-6 md:flex">
+            {items?.map((item, index) => (
+              <Link
+                key={index}
+                href={item.disabled ? "#" : item.href}
+                className={cn(
+                  "flex items-center text-lg font-medium transition-colors hover:text-foreground/80 sm:text-sm",
+                  item.href.startsWith(`/${segment}`)
+                    ? "text-foreground"
+                    : "text-foreground/60",
+                  item.disabled && "cursor-not-allowed opacity-80",
+                )}
+              >
+                {item.title}
+              </Link>
+            ))}
+          </nav>
+        ) : null}
+      </div>
+
+      {/* RIGHT */}
+      <div className="md:hidden w-6/12 flex items-center justify-end">
         <button
           className="flex items-center space-x-2"
           onClick={() => setShowMobileMenu(!showMobileMenu)}
@@ -61,5 +58,5 @@ export function MainNav({ items, children }: MainNavProps) {
         )}
       </div>
     </div>
-  )
+  );
 }

fb45cafb62bb38e8ae597b2ad1ed84e26203e64c
Robert Sturla <robertsturla@outlook.com>
2024-02-27 18:44:16 +0000

    Adapt frontend with new count API

--- web/app/(dashboard)/page.tsx
+++ web/app/(dashboard)/page.tsx
@@ -1,25 +1,35 @@
 "use client";
 import useSWR from "swr";
 
-async function fetcher(url: string) {
+interface Data {
+  count: number;
+}
+
+async function fetcher(url: string): Promise<string> {
   const resp = await fetch(url);
+  if (!resp.ok) {
+    throw new Error("Failed to fetch data");
+  }
   return resp.text();
 }
 
 export default function Home() {
-  const { data, error } = useSWR("/api", fetcher, { refreshInterval: 1000 });
+  const { data, error } = useSWR("/api/count", fetcher, { refreshInterval: 1000 });
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  const parsedData: Data = JSON.parse(data);
 
   return (
     <div>
-      <h1>Hello World!</h1>
-      <h2>Memory allocation stats from GoLang API</h2>
-      {error && (
-        <div>
-          Failed to load: <strong>{error}</strong>
-        </div>
-      )}
-      {!error && !data && <p>Loading...</p>}
-      {!error && data && <pre>{data}</pre>}
+      <h2>Counter service from GoLang API</h2>
+      <h1>Count: {parsedData.count}</h1>
     </div>
   );
 }
